d2b7030aed903af1988e2d1d39ebc5bddcbd796d
рума <elchaninapolina@gmail.com>
2025-02-14 02:04:12 +0700

    kapec

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,35 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Collect\Collect; // Убедитесь, что класс правильно подключен
 
-class CollectTest extends TestCase
-{
-    public function testCount()
+class CollectTest extends TestCase {
+
+    public function testPop()
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $collect = new Collect([123, 98, 5]);
+        $collect->pop();
+        $this->assertSame([123, 98], $collect->toArray());
     }
 
+    public function testSplice()
+    {
+        $collect = new Collect(['a', 'b', 'c', 'd', 'e']);
+        // Исправленный вызов splice: удаляем 2 элемента начиная с индекса 1
+        $collect->splice(1, 2);
+        $this->assertSame(['a', 'd', 'e'], $collect->toArray());
+    }
+
+    public function testKeys()
+    {
+        $collect = new Collect(['qwe' => 1, 'asd' => 2]);
+        // Приведение к массиву на случай, если keys() возвращает объект Collect
+        $this->assertSame(['qwe', 'asd'], array_values($collect->keys()->toArray()));
+    }
+
+    public function testFirst()
+    {
+        $collect = new Collect(['qwe' => 1, 'asd' => 2]);
+        $this->assertSame(1, $collect->first());
+    }
 }
